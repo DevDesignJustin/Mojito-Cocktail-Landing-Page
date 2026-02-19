6801ea227bec9048e6df42fb0fabfa792d5a28b1
Justin <devdesignjustin@gmail.com>
2026-02-18 19:03:07 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+const App = () => {
+  return <div className="bg-red-500">App</div>;
+};
+
+export default App;
